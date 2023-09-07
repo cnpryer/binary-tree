a338bb7db8ef82052ef64fc63b953806f4cfd2b7
Chris Pryer <cnpryer@gmail.com>
2023-09-07 17:47:36 -0400

    Update tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,7 +2,13 @@ from binary_tree import Tree
 from binary_tree.utils import assert_tree_values
 
 
-def test_from_values() -> None:
+def test_tree_from_values() -> None:
     values = [1, 2, 3, 5]
     tree = Tree.from_values(values)
     assert_tree_values(tree, values)
+
+
+def test_tree_inverted() -> None:
+    values = [1, 2, 3, 5]
+    tree = Tree.from_values(values).inverted()
+    assert_tree_values(tree, [1, 3, 2, None, None, None, 5])
